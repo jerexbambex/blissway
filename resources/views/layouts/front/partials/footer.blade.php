9ba2cc451aa0c85fa151a893215626593e5f76f7
Oluwatosin Jeremiah <jerexbambex@gmail.com>
2023-09-23 11:34:07 -0500

    assets added with homepage almost complete

---
+++ resources/views/layouts/front/partials/footer.blade.php
@@ -0,0 +1,74 @@
+<footer class="bg-dark text-inverse">
+    <div class="container pt-13 pt-md-15 pb-7">
+        <div class="row gy-6 gy-lg-0">
+            <div class="col-lg-4">
+                <div class="widget">
+                    <img src="./assets/img/photos/blissway-logo.png" class="svg-inject icon-svg text-aqua" style="width: auto !important; height: 3rem;" alt="" />
+                    <h3 class="h2 mb-3 text-white">Blissway Immigrations</h3>
+                    <p class="lead mb-5">We are leading Education Consulting Firm that has been serving aspiring students since 2018 to Study Abroad.</p>
+                    <div class="d-flex flex-row">
+                        <div>
+                            <img src="./assets/img/photos/iso.png" class="svg-inject icon-svg text-aqua me-4" alt="" />
+                        </div>
+                        <div>
+                            <img src="./assets/img/photos/cicc.png" class="svg-inject icon-svg text-aqua me-4" style="width: auto !important; height: 3rem;" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <!-- /.widget -->
+            </div>
+            <!-- /column -->
+            <div class="col-md-4 col-lg-2 offset-lg-2">
+                <div class="widget">
+                    <h4 class="widget-title text-white mb-3">Need Help?</h4>
+                    <ul class="list-unstyled text-reset mb-0">
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Get Started</a></li>
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+                <!-- /.widget -->
+            </div>
+            <!-- /column -->
+            <div class="col-md-4 col-lg-2">
+                <div class="widget">
+                    <h4 class="widget-title text-white mb-3">Learn More</h4>
+                    <ul class="list-unstyled  mb-0">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Our Story</a></li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Features</a></li>
+                    </ul>
+                </div>
+                <!-- /.widget -->
+            </div>
+            <!-- /column -->
+            <div class="col-md-4 col-lg-2">
+                <div class="widget">
+                    <h4 class="widget-title text-white mb-3">Get in Touch</h4>
+                    <address>Moonshine St. 14/05 Light City, London, United Kingdom</address>
+                    <a href="mailto:first.last@email.com">info@email.com</a><br /> 00 (123) 456 78 90
+                </div>
+                <!-- /.widget -->
+            </div>
+            <!-- /column -->
+        </div>
+        <!--/.row -->
+        <hr class="mt-13 mt-md-15 mb-7" />
+        <div class="d-md-flex align-items-center justify-content-between">
+            <p class="mb-2 mb-lg-0">© 2023 Sandbox. All rights reserved.</p>
+            <nav class="nav social social-white text-md-end">
+                <a href="#"><i class="uil uil-twitter"></i></a>
+                <a href="#"><i class="uil uil-facebook-f"></i></a>
+                <a href="#"><i class="uil uil-dribbble"></i></a>
+                <a href="#"><i class="uil uil-instagram"></i></a>
+                <a href="#"><i class="uil uil-youtube"></i></a>
+            </nav>
+            <!-- /.social -->
+        </div>
+        <!-- /div -->
+    </div>
+    <!-- /.container -->
+</footer>
---
+++ resources/views/layouts/front/template.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="An impressive and flawless site template that includes various UI elements and countless features, attractive ready-made blocks and rich pages, basically everything you need to create a unique and professional website.">
+    <meta name="keywords" content="bootstrap 5, business, corporate, creative, gulp, marketing, minimal, modern, multipurpose, one page, responsive, saas, sass, seo, startup, html5 template, site template">
+    <meta name="author" content="elemis">
+    <title>.::Blissway - @yield('title')</title>
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/colors/red.css') }}">
+    <link rel="preload" href="{{ asset('assets/css/fonts/urbanist.css') }}" as="style" onload="this.rel='stylesheet'">
+</head>
+
+<body>
+<div class="content-wrapper">
+    @include('layouts.front.partials.nav')
+    <!-- /header -->
+    @yield('body')
+</div>
+<!-- /.content-wrapper -->
+@include('layouts.front.partials.footer')
+<div class="progress-wrap">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    </svg>
+</div>
+<script src="{{ asset('assets/js/plugins.js') }}"></script>
+<script src="{{ asset('assets/js/theme.js') }}"></script>
+</body>
+
+</html>
